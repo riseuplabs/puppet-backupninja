6406454bbf71595c511d8ccf838a6007b4fe1e5e
Micah Anderson <micah@riseup.net>
2008-05-17 23:31:38 +0000

    create a backupninja svn piece to the module

---
+++ manifests/svn.pp
@@ -0,0 +1,32 @@
+# Subversion dumps, as part of a backupninja run.
+#
+# Valid attributes for this type are:
+#
+#   order: The prefix to give to the handler config filename, to set
+#      order in which the actions are executed during the backup run.
+#
+#   ensure: Allows you to delete an entry if you don't want it any more
+#      (but be sure to keep the configdir, name, and order the same, so
+#      that we can find the correct file to remove).
+#
+#   user, dbusername, dbpassword, dbhost, databases, backupdir,
+#   hotcopy, sqldump, compress, configfile: As defined in the
+#   backupninja documentation, with the caveat that hotcopy, sqldump,
+#   and compress take true/false rather than yes/no.
+# 
+define backupninja::svn($order = 20,
+                           $ensure = present,
+                           $src = '/var/lib/svn',
+                           $dest = '/var/backups/svn',
+                           $tmp = '/var/backups/svn.tmp',
+                           $vsname = false,
+                          ) {
+	file { "${backupninja::client::configdir}/${order}_${name}.svn":
+		ensure => $ensure,
+		content => template('backupninja/svn.conf.erb'),
+		owner => root,
+		group => root,
+		mode => 0600,
+		require => File["${backupninja::client::configdir}"]
+	}
+}
