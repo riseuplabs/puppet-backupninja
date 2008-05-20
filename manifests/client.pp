d9b765d66ef377532b803d570af27b5dcb2a119a
Micah Anderson <micah@riseup.net>
2008-05-20 20:31:05 +0000

    harmonize spacing to be consistant

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,12 +1,12 @@
 class backupninja::client {
-        $configdir = $cfg_override ? {
-	        '' => "/etc/backup.d",
-		default => $cfg_override,
-        }
-        package { 'backupninja':
-	        ensure => '0.9.5-3';
-        }
-        file { $configdir:
-                ensure => directory
-        }
+  $configdir = $cfg_override ? {
+    '' => "/etc/backup.d",
+    default => $cfg_override,
+  }
+  package { 'backupninja':
+    ensure => '0.9.5-3';
+  }
+  file { $configdir:
+    ensure => directory
+  }
 }
--- manifests/server.pp
+++ manifests/server.pp
@@ -1,63 +1,63 @@
 class backupninja::server {
-    $backupdir = $backupdir_override ? {
-            '' => "/backup",
-	    default => $backupdir_override,
-    }
-    group { "backupninjas":
-            ensure => "present",
-            gid => 700
-    }
-    file { "$backupdir":
-            ensure => "directory",
-            mode => 710, owner => root, group => "backupninjas"
-    }
-    User <<| tag == "backupninja-$fqdn" |>>
-    File <<| tag == "backupninja-$fqdn" |>>
+  $backupdir = $backupdir_override ? {
+    '' => "/backup",
+    default => $backupdir_override,
+  }
+  group { "backupninjas":
+    ensure => "present",
+    gid => 700
+  }
+  file { "$backupdir":
+    ensure => "directory",
+    mode => 710, owner => root, group => "backupninjas"
+  }
+  User <<| tag == "backupninja-$fqdn" |>>
+  File <<| tag == "backupninja-$fqdn" |>>
 
-    # this define allows nodes to declare a remote backup sandbox, that have to
-    # get created on the server
-    define sandbox($host = false, $dir = false, $uid = false, $gid = "backupninjas") {
-        $real_host = $host ? {
-	    false => $fqdn,
-	    default => $host,
-	}
-        $real_dir = $dir ? {
-	    false => "${backupninja::server::backupdir}/$fqdn",
-	    default => $dir,
-	}
-      	@@file { "$real_dir":
-	    ensure => "directory",
-	    mode => 750, owner => $name, group => 0,
-            tag => "backupninja-$real_host",
-	}
-        case $uid {
-            false: {
-                @@user { "$name":
-                    ensure  => "present",
-                    gid     => "$gid",
-                    comment => "$name backup sandbox",
-                    home    => "$real_dir",
-                    managehome => true,
-                    shell   => "/bin/sh",
-                    password => '*',
-                    require => Group['backupninjas'],
-                    tag => "backupninja-$real_host"
-                }
-            }
-            default: {
-                @@user { "$name":
-                    ensure  => "present",
-                    uid     => "$uid",
-                    gid     => "$gid",
-                    comment => "$name backup sandbox",
-                    home    => "$real_dir",
-                    managehome => true,
-                    shell   => "/bin/sh",
-                    password => '*',
-                    require => Group['backupninjas'],
-                    tag => "backupninja-$real_host"
-                }
-            }
-        }
+  # this define allows nodes to declare a remote backup sandbox, that have to
+  # get created on the server
+  define sandbox($host = false, $dir = false, $uid = false, $gid = "backupninjas") {
+    $real_host = $host ? {
+      false => $fqdn,
+      default => $host,
     }
+    $real_dir = $dir ? {
+      false => "${backupninja::server::backupdir}/$fqdn",
+      default => $dir,
+    }
+    @@file { "$real_dir":
+      ensure => "directory",
+      mode => 750, owner => $name, group => 0,
+      tag => "backupninja-$real_host",
+    }
+    case $uid {
+      false: {
+        @@user { "$name":
+          ensure  => "present",
+          gid     => "$gid",
+          comment => "$name backup sandbox",
+          home    => "$real_dir",
+          managehome => true,
+          shell   => "/bin/sh",
+          password => '*',
+          require => Group['backupninjas'],
+          tag => "backupninja-$real_host"
+        }
+      }
+      default: {
+        @@user { "$name":
+          ensure  => "present",
+          uid     => "$uid",
+          gid     => "$gid",
+          comment => "$name backup sandbox",
+          home    => "$real_dir",
+          managehome => true,
+          shell   => "/bin/sh",
+          password => '*',
+          require => Group['backupninjas'],
+          tag => "backupninja-$real_host"
+        }
+      }
+    }
+  }
 }
