cbcf8596df69094372d0c54d86b1f38a96b55086
Micah Anderson <micah@riseup.net>
2008-05-22 03:48:54 +0000

    oops, one more curly brace fix

--- manifests/client.pp
+++ manifests/client.pp
@@ -34,24 +34,24 @@ class backupninja::client {
     ensure => directory
   }
   define key 
-    ( $sourcekey='$real_keystore/${name}_id_$real_keytype',
-    $destination='$real_keydestination', $key_type=$real_keytype,
-    $key_owner=$real_keyowner, $key_group=$real_keygroup, $installkey=$real_keymanage )
+  ( $sourcekey='$real_keystore/${name}_id_$real_keytype',
+  $destination='$real_keydestination', $key_type=$real_keytype,
+  $key_owner=$real_keyowner, $key_group=$real_keygroup, $installkey=$real_keymanage )
   {
     case $installkey {
       true: {
-        {
-          file { "$destination":
-            ensure => directory,
-            mode => 700, owner => $keyowner, group => $keygroup,
-          }
-          file { "$destination/id_$keytype":
-            source => "$sourcekey",
-            mode => 0400, owner => $keyowner, group => $keygroup,
-            require => File["$destination"],
-          }
+        file { "$destination":
+          ensure => directory,
+          mode => 700, owner => $keyowner, group => $keygroup,
+        }
+        file { "$destination/id_$keytype":
+          source => "$sourcekey",
+          mode => 0400, owner => $keyowner, group => $keygroup,
+          require => File["$destination"],
         }
       }
     }
   }
 }
+
+
