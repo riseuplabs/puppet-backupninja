911b251f1acfdf8d8617ded71e2d7616b6580484
Micah Anderson <micah@riseup.net>
2008-05-22 03:46:11 +0000

    fix missing curly braces, and also fix up indentation

--- manifests/client.pp
+++ manifests/client.pp
@@ -33,22 +33,23 @@ class backupninja::client {
   file { $configdir:
     ensure => directory
   }
-  define key (
-    $sourcekey='$real_keystore/${name}_id_$real_keytype',
+  define key 
+    ( $sourcekey='$real_keystore/${name}_id_$real_keytype',
     $destination='$real_keydestination', $key_type=$real_keytype,
-    $key_owner=$real_keyowner, $key_group=$real_keygroup, $installkey=$real_keymanage
-  )
-  case $installkey {
-    true: {
-      {
-        file { "$destination":
-          ensure => directory,
-          mode => 700, owner => $keyowner, group => $keygroup,
-        }
-        file { "$destination/id_$keytype":
-          source => "$sourcekey",
-          mode => 0400, owner => $keyowner, group => $keygroup,
-          require => File["$destination"],
+    $key_owner=$real_keyowner, $key_group=$real_keygroup, $installkey=$real_keymanage )
+  {
+    case $installkey {
+      true: {
+        {
+          file { "$destination":
+            ensure => directory,
+            mode => 700, owner => $keyowner, group => $keygroup,
+          }
+          file { "$destination/id_$keytype":
+            source => "$sourcekey",
+            mode => 0400, owner => $keyowner, group => $keygroup,
+            require => File["$destination"],
+          }
         }
       }
     }
