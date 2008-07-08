6b09aba10d292c47423cbab283f89f2490b2a3f8
Pietro <pietro@riseup.net>
2008-07-08 18:43:42 +0000

    sh backupninja handler.

---
+++ manifests/sh.pp
@@ -0,0 +1,26 @@
+# sh handler, as part of a backupninja run.
+#
+# Valid attributes for this type are:
+#
+#   order: The prefix to give to the handler config filename, to set
+#      order in which the actions are executed during the backup run.
+#
+#   ensure: Allows you to delete an entry if you don't want it any more
+#      (but be sure to keep the configdir, name, and order the same, so
+#      that we can find the correct file to remove).
+#
+# 
+define backupninja::sh($order = 30,
+                           $ensure = present,
+                           $command_string
+                          ) {
+                          include backupninja::client
+	file { "${backupninja::client::configdir}/${order}_${name}.sh":
+		ensure => $ensure,
+		content => template('backupninja/sh.conf.erb'),
+		owner => root,
+		group => root,
+		mode => 0600,
+		require => File["${backupninja::client::configdir}"]
+	}
+}
