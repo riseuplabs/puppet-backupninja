7f7101d5cd099dcd941682c6db6d36671e11870c
Micah Anderson <micah@riseup.net>
2008-07-31 14:03:11 +0000

    change to standardized formatting

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,26 +2,19 @@
 # little more than just take the config options you specify in the define and
 # write them to the config file as-is.
 #
-define backupninja::config($configfile = '/etc/backupninja.conf',
-                            $loglvl = 4,
-                            $when = '01:00',
-                            $reportemail = 'root',
-                            $reportsuccess = false,
-                            $reportwarning = true,
-                            $reporthost = $reporthost,
-                            $reportuser = $reportuser,
-                            $reportdirectory = $reportdirectory,
-                            $logfile = '/var/log/backupninja.log',
-                            $configdir = '/etc/backup.d',
-                            $scriptdir = '/usr/share/backupninja',
-                            $libdir = '/usr/lib/backupninja',
-                            $usecolors = true,
-                            $vservers = false
-             ) {
-	file { $configfile:
-		content => template('backupninja/backupninja.conf.erb'),
-		owner => root,
-		group => root,
-		mode => 0644
-	}
+define backupninja::config(
+  $configfile = '/etc/backupninja.conf', $loglvl = 4, $when = '01:00',
+  $reportemail = 'root', $reportsuccess = false, $reportwarning = true,
+  $reporthost = $reporthost, $reportuser = $reportuser,
+  $reportdirectory = $reportdirectory,
+  $logfile = '/var/log/backupninja.log', $configdir = '/etc/backup.d',
+  $scriptdir = '/usr/share/backupninja', $libdir = '/usr/lib/backupninja',
+  $usecolors = true, $vservers = false)
+{
+  file { $configfile:
+    content => template('backupninja/backupninja.conf.erb'),
+    owner => root,
+    group => root,
+    mode => 0644
+  }
 }
