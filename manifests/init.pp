4c591e339655f7228b4a7d4e5632d4574dd35a99
drebs <drebs@riseup.net>
2009-07-13 04:13:15 -0300

    eliminating some globbing because of server cache.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,16 @@
-import "*.pp"
+import "client.pp"
+import "config.pp"
+import "dup.pp"
+import "labelmount.pp"
+import "maildir.pp"
+import "mysql.pp"
+import "rdiff.pp"
+import "server.pp"
+import "sh.pp"
+import "svn.pp"
+import "sys.pp"
 
 class backupninja {
 
 }
+
