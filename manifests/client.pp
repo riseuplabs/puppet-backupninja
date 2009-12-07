ad99d7611f716eada0620417f7d62c095ef38fd1
Varac <varacanero@zeromail.org>
2009-12-07 01:46:52 +0100

    Added class backupninja::client::sys to install neccessary files for backupninja::sys on debian and ubuntu.

--- manifests/client.pp
+++ manifests/client.pp
@@ -127,4 +127,23 @@ class backupninja::client::duplicity inherits backupninja::client::defaults {
     }
   }
 }
-  
+
+class backupninja::client::sys inherits backupninja::client::defaults {
+  case $operatingsystem {
+    debian,ubuntu: {
+      if !defined(Package["debconf-utils"]) {
+	if $debconf_utils_ensure_version == '' { $debconf_utils_ensure_version = 'installed' }
+	package { 'debconf-utils':
+	  ensure => $debconf_utils_ensure_version,
+	}
+      }
+      if !defined(Package["hwinfo"]) {
+	if $hwinfo_ensure_version == '' { $hwinfo_ensure_version = 'installed' }
+	package { 'hwinfo':
+	  ensure => $hwinfo_ensure_version,
+	}
+      }
+    }
+    default: {}
+  }
+}  
